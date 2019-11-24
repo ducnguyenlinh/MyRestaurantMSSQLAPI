d2992cedce004499c10fb22218df4d326763c78f
ducnguyenlinh <duclinh0305@gmail.com>
2019-11-24 18:12:07 +0900

    userのAPIを更新する

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var API_KEY = "1234";
 
-var express = require('express')
+var express = require('express');
 var router = express.Router();
-const { poolPromise, sql } = require('../db')
+const { poolPromise, sql } = require('../db');
 
 /*
  * TEST API
@@ -15,74 +15,76 @@ router.get('/', function (req, res) {
 //============================================
 // USER TABLE
 // POST / GET
+// GET /user
+// POST /user
 //============================================
 router.get('/user', async(req, res, next) => {
     console.log(req.query);
     
     if (req.query.key != API_KEY) {
-        res.send(JSON.stringify({ success: false, message: "Wrong API key" }));
+        res.send(JSON.stringify({success: false, message: "Wrong API key"}));
     } else {
         var fbid = req.query.fbid;
         if (fbid != null) {
             try {
-                const pool = await poolPromise
+                const pool = await poolPromise;
                 const queryResult = await pool.request()
                     .input('fbid', sql.NVarChar, fbid)
-                    .query('SELECT userPhone,name,address,fbid FROM [User] where fbid=@fbid')
+                    .query('SELECT UserPhone,Name,Address,FBID FROM [User] where FBID=@fbid');
                 if (queryResult.recordset.length > 0) {
-                    res.send(JSON.stringify({ success: true, result: queryResult.recordset }));
+                    res.send(JSON.stringify({success: true, result: queryResult.recordset}));
                 } else {
-                    res.send(JSON.stringify({ success: false, message: "Empty" }));
+                    res.send(JSON.stringify({success: false, message: "Empty"}));
                 }
             } catch (err) {
-                res.status(500) // Internal Server Error
-                res.send(JSON.stringify({ success: false, message: err.message }));
+                res.status(500); // Internal Server Error
+                res.send(JSON.stringify({success: false, message: err.message}));
             }
         } else {
-            res.send(JSON.stringify({ success: false, message: "Missing fbid in query" }));
+            res.send(JSON.stringify({success: false, message: "Missing fbid in query"}));
         }
     }
-})
+});
 
 router.post('/user', async(req, res, next) => {
-    console.log(req.body)
+    console.log(req.body);
 
     if (req.body.key != API_KEY) {
-        res.send(JSON.stringify({ success: false, message: "Wrong API key" }));
+        res.send(JSON.stringify({success: false, message: "Wrong API key"}));
     } else {
-        var user_phone = req.body.userPhone
-        var user_name = req.body.userName
-        var user_address = req.body.userAddress
-        var fbid = req.body.fbid
+        var user_phone = req.body.userPhone;
+        var user_name = req.body.userName;
+        var user_address = req.body.userAddress;
+        var fbid = req.body.fbid;
 
         if (fbid != null) {
             try {
-                const pool = await poolPromise
+                const pool = await poolPromise;
                 const queryResult = await pool.request()
-                    .input('UserPhone', sql.NVarChar, user_phone)
-                    .input('UserName', sql.NVarChar, user_name)
-                    .input('UserAddress', sql.NVarChar, user_address)
-                    .input('FBID', sql.NVarChar, fbid)
-                    .query('IF EXISTS(SELECT * FROM [User] WHERE FBID=@FBID)'
-                        + ' UPDATE [User] SET Name=@UserName, Address=@UserAddress WHERE FBID=@FBID'
+                    .input('userPhone', sql.NVarChar, user_phone)
+                    .input('userName', sql.NVarChar, user_name)
+                    .input('userAddress', sql.NVarChar, user_address)
+                    .input('fbid', sql.NVarChar, fbid)
+                    .query('IF EXISTS(SELECT * FROM [User] WHERE FBID=@fbid)'
+                        + ' UPDATE [User] SET Name=@userName, Address=@userAddress WHERE FBID=@fbid'
                         + ' ELSE'
-                        + ' INSERT INTO [User](FBID, UserPhone, Name, Address) OUTPUT Inserted.FBID, Inserted.UserPhone, Inserted.Name, Inserted.Address'
-                        + ' VALUES(@FBID, @UserPhone, @UserName, @UserAddress)'
-                );
+                        + ' INSERT INTO [User](FBID,UserPhone,Name,Address) OUTPUT Inserted.FBID,Inserted.UserPhone,Inserted.Name,Inserted.Address'
+                        + ' VALUES(@fbid, @userPhone, @userName, @userAddress)'
+                    );
 
                 console.log(queryResult); // Debug to see
-                
+
                 if (queryResult.rowsAffected != null) {
-                    res.send(JSON.stringify({ success: true, message: "Success" }));
+                    res.send(JSON.stringify({success: true, message: "Success"}));
                 }
             } catch (err) {
-                res.status(500) // Internal Server Error
-                res.send(JSON.stringify({ success: false, message: err.message }));
+                res.status(500); // Internal Server Error
+                res.send(JSON.stringify({success: false, message: err.message}));
             }
         } else {
-            res.send(JSON.stringify({ success: false, message: "Missing fbid in body of POST query" }));
+            res.send(JSON.stringify({success: false, message: "Missing fbid in body of POST query"}));
         }
     }
-})
+});
 
 module.exports = router;
